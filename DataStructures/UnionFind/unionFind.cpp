01aee63c6afc10634744197b25848f8e1f5ba61d
Rene Rengel <Rene.Rengel@jala.university>
2022-09-27 10:27:29 -0400

    Backtracking

---
+++ DataStructures/UnionFind/unionFind.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
+using namespace std;
+
+
+int persons[100];
+
+
+
+
+int main() {
+    input;
+    cin>>n, m;
+    init();
+    vector<pair<int, int>> threads (m);
+    for(int i = 0; i < dele){
+    int x, y;
+    cin>>x, y;
+    threads[i] = make_pair(x, y);
+    }
+    
+
+    return 0;
+}
